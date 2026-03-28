b412e85bd18a3c4437aa96c1eab13fcd9cd99ff4
LeninTF <lenin7777.lt@gmail.com>
2026-03-28 02:01:49 -0500

    Ejercicio acceso a bd

---
+++ Acceso a Base de Datos/build/web/registroBorrado.jsp
@@ -0,0 +1,16 @@
+<%-- 
+    Author     : Lenin Tambra
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%> 
+<!DOCTYPE html> 
+<html> 
+    <head> 
+        <meta http-equiv="Content-Type" 
+                 content="text/html; charset=UTF-8"> 
+        <title>JSP Page</title> 
+    </head> 
+    <body> 
+        <h1>El resgistro se borró con éxito</h1> 
+    </body> 
+</html> 
---
+++ Acceso a Base de Datos/src/java/Model/ConectaBD.java
@@ -0,0 +1,34 @@
+package Model; 
+import java.sql.Connection; 
+import java.sql.DriverManager; 
+ 
+public class ConectaBD { 
+    public static Connection con=null; 
+    private static String bd = "sistemalogin"; 
+    public static String usuario = "admin"; 
+    public static String passw = "admin"; 
+    public static String url = "jdbc:mysql://localhost:3306/" + bd + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+ 
+    public static Connection abrir(){ 
+        try{ 
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
+            con = DriverManager.getConnection(url,usuario,passw); 
+            System.out.println("Conexión exitosa:"+con); 
+             
+        } catch (Exception e) { 
+            System.out.println("Error en la conexion..."); 
+            e.printStackTrace(); 
+            return null; 
+        } 
+        return con; 
+    } 
+     
+    public static void cerrar(){ 
+        try{ 
+            if(con != null) 
+                con.close(); 
+        } catch (Exception e){ 
+            System.out.println("Error: No se logro cerrar la conexion:\n"+e); 
+        } 
+    }   
+} 
